c1fcff8a95120b745172e4a71c04b93bca3ff6ef
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-09-08 10:56:18 +0530

    updated application

--- backend/src/config/sequelize-cli.js
+++ backend/src/config/sequelize-cli.js
@@ -12,7 +12,7 @@ module.exports = {
     logging: false
   },
   production: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL | "postgresql://hireflow:hireflow_password@postgres:5432/hireflow",
     dialect: "postgres",
     logging: false,
     dialectOptions: {
